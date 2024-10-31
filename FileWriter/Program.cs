791a158f1a71054a6ff8af5c2ced49f7524921da
Julian Bliemel <Julian.Bliemel@drheuer.de>
2024-10-31 15:06:25 +0100

    Add project files.

---
+++ FileWriter/Program.cs
@@ -0,0 +1,66 @@
+﻿using System.Windows.Forms;
+
+namespace FileWriter;
+
+internal static class Program
+{
+    [STAThread]
+    static void Main()
+    {
+        string? path;
+
+        OpenFileDialog openFile = new();
+
+        while ( true )
+        {
+            Console.Clear();
+
+            if ( openFile.ShowDialog() == DialogResult.OK )
+            {
+                path = openFile.FileName;
+                break;
+            }
+            else
+            {
+                Console.WriteLine( "No file selected." );
+                Console.WriteLine( $"\n<Esc> Beenden\n<Backspace> Enter path\n<Enter> Open File Dialog" );
+                ConsoleKeyInfo key = Console.ReadKey();
+
+                if ( key.Key == ConsoleKey.Escape )
+                    Application.Exit();
+
+                if ( key.Key == ConsoleKey.Backspace )
+                {
+                    Console.Write( "\rPath: " );
+                    path = Console.ReadLine();
+                    break;
+                }
+
+                if ( key.Key == ConsoleKey.Enter )
+                    continue;
+            }
+        }
+
+        while ( true )
+        {
+            Console.Clear();
+
+            string? input = Console.ReadLine();
+
+            if ( string.IsNullOrWhiteSpace( input ) )
+                continue;
+
+            if ( input == "." )
+                break;
+
+            File.AppendAllText( path , DateTime.Now.ToLongTimeString() + " " + input + "\n" );
+        }
+
+        Console.Clear();
+
+        foreach ( string line in File.ReadAllLines( path ) )
+            Console.WriteLine( line );
+
+        Console.ReadLine();
+    }
+}
---
+++ LookUp/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Net;
+
+FileSystemWatcher spy = new()
+{
+    Path = @"C:\Lookup" ,
+    Filter = @"*.lookup" ,
+    NotifyFilter = NotifyFilters.LastWrite | NotifyFilters.FileName | NotifyFilters.CreationTime ,
+    EnableRaisingEvents = true
+};
+
+spy.Created += new FileSystemEventHandler( OnFileCreated );
+
+Console.WriteLine( "Watching for .lookup files. Press enter to exit." );
+Console.ReadLine();
+
+static void OnFileCreated( object source , FileSystemEventArgs e )
+{
+    string [] lines = File.ReadAllLines( e.FullPath );
+    string resolvedPath = e.FullPath.Replace( ".lookup" , ".resolved" );
+
+    using ( StreamWriter writer = new StreamWriter( resolvedPath , false ) )
+    {
+        foreach ( string line in lines )
+        {
+            string [] fqdn = line.Split( '.' );
+
+            if ( fqdn.Length < 3 )
+            {
+                writer.WriteLine( $"# {line} (invalid FQDN)" );
+                continue;
+            }
+
+            writer.WriteLine( $"# {line}" );
+
+            IPAddress [] adresses = Dns.GetHostAddresses( line );
+
+            foreach ( IPAddress address in adresses )
+                writer.WriteLine( address.ToString() );
+
+            writer.WriteLine();
+        }
+    }
+
+    Console.WriteLine( $"Processed file: {e.Name}" );
+}
